62a1fba2327534ec694408cc61a9d9cd99c64a2a
Ian Marcelo <ianmarcelo02@gmail.com>
2020-11-10 13:33:15 -0500

    Added scoreboard function

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-// Create Starting Player Variable 
-// Create random number generator, generate num between 0 and 1
-// Store randomly generated number in starting player variable
-let startPlayer = Math.round(Math.random() * 1);
-console.log(startPlayer);
+// Create win counter
+    // Create win count variables for x and o
+    let x_wins = 0;
+    let o_wins = 0;
+    // Create querySelector for score div
+    let score = document.querySelector('#score');
+    // Create querySelector for x wins and o wins
+    let x_score = document.querySelector('#x_score');
+    let o_score = document.querySelector('#o_score');
 
-// If number = 0, O is up; If number = 1, X is up 
-let player;
-startPlayer == 0 ? player = "O" : player = "X"; 
-console.log(player);
+    // Place current scores in score board 
+    // Create score board function; 
+    const scoreBoard = () => {
+        x_score.textContent = `X Wins: ${x_wins}`;
+        o_score.textContent = `O Wins: ${o_wins}`; 
+    };
+
+// Create a function for start of game
+const start = () => {
+    // Create Starting Player Variable 
+    // Create random number generator, generate num between 0 and 1
+    // Store randomly generated number in starting player variable
+    let startPlayer = Math.round(Math.random() * 1);
+    console.log(startPlayer);
+
+    // If number = 0, O is up; If number = 1, X is up 
+    let player;
+    startPlayer == 0 ? player = "O" : player = "X"; 
+    console.log(player);
+
+    // Call scoreBoard Function 
+    scoreBoard();
+};
+
+start();
