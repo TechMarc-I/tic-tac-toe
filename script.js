7d186ff033fe8ebe7a6b11f8b03cccd5cdf41afa
Ian Marcelo <ianmarcelo02@gmail.com>
2020-11-11 21:19:25 -0500

    Added Click Events for column A and B

--- script.js
+++ script.js
@@ -1,3 +1,127 @@
+// variable to track which player goes next
+let player;
+let startPlayer;
+
+// create function to update current player
+const playerTurn = () => {
+    // If number = 0, O is up; If number = 1, X is up
+    startPlayer == 0 ? player = "O" : player = "X"; 
+    console.log(player);
+}
+
+
+// Variables to track each indiviual div
+// Column A
+let a1 = document.querySelector('#a1');
+let a2 = document.querySelector('#a2');
+let a3 = document.querySelector('#a3');
+//Column B
+let b1 = document.querySelector('#b1');
+let b2 = document.querySelector('#b2');
+let b3 = document.querySelector('#b3');
+//Column C
+let c1 = document.querySelector('#c1');
+let c2 = document.querySelector('#c2');
+let c3 = document.querySelector('#c3');
+
+// Create click events to place either x or o
+// on click, either an x or an o should appear in div
+// after x or o appears, ready up next letter
+// A1
+a1.addEventListener('click', function () {
+    a1.textContent = player; 
+
+    if (startPlayer === 0) {
+        startPlayer += 1;
+    } else {
+        startPlayer -= 1;
+    };
+
+    playerTurn();
+    console.log(startPlayer);
+
+    a1.style.pointerEvents = 'none';
+})
+// A2
+a2.addEventListener('click', function () {
+    a2.textContent = player; 
+
+    if (startPlayer === 0) {
+        startPlayer += 1;
+    } else {
+        startPlayer -= 1;
+    };
+
+    playerTurn();
+    console.log(startPlayer);
+
+    a2.style.pointerEvents = 'none';
+})
+// A3
+a3.addEventListener('click', function () {
+    a3.textContent = player; 
+
+    if (startPlayer === 0) {
+        startPlayer += 1;
+    } else {
+        startPlayer -= 1;
+    };
+
+    playerTurn();
+    console.log(startPlayer);
+
+    a3.style.pointerEvents = 'none';
+})
+// B1
+b1.addEventListener('click', function () {
+    b1.textContent = player; 
+
+    if (startPlayer === 0) {
+        startPlayer += 1;
+    } else {
+        startPlayer -= 1;
+    };
+
+    playerTurn();
+    console.log(startPlayer);
+
+    b1.style.pointerEvents = 'none';
+})
+// B2
+b2.addEventListener('click', function () {
+    b2.textContent = player; 
+
+    if (startPlayer === 0) {
+        startPlayer += 1;
+    } else {
+        startPlayer -= 1;
+    };
+
+    playerTurn();
+    console.log(startPlayer);
+
+    b2.style.pointerEvents = 'none';
+})
+// B3
+b3.addEventListener('click', function () {
+    b3.textContent = player; 
+
+    if (startPlayer === 0) {
+        startPlayer += 1;
+    } else {
+        startPlayer -= 1;
+    };
+
+    playerTurn();
+    console.log(startPlayer);
+
+    b3.style.pointerEvents = 'none';
+})
+
+
+
+
+
 // Create win counter
     // Create win count variables for x and o
     let x_wins = 0;
@@ -20,11 +144,11 @@ const start = () => {
     // Create Starting Player Variable 
     // Create random number generator, generate num between 0 and 1
     // Store randomly generated number in starting player variable
-    let startPlayer = Math.round(Math.random() * 1);
+    startPlayer = Math.round(Math.random() * 1);
     console.log(startPlayer);
 
-    // If number = 0, O is up; If number = 1, X is up 
-    let player;
+    //call playerTurn function
+    // If number = 0, O is up; If number = 1, X is up
     startPlayer == 0 ? player = "O" : player = "X"; 
     console.log(player);
 
